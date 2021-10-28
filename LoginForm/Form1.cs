b6de63f4395b0689c8f3ce1ca5e945eba07116ba
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2021-10-28 08:12:11 +0600

    Initial commit

---
+++ LoginForm/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Lenovo\Documents\dbForm.mdf; Integrated Security = True; Connect Timeout = 30");
+            SqlDataAdapter sqa = new SqlDataAdapter("SELECT count(*) FROM TableLogin WHERE UserName ='" + textUserName.Text + "' and Password = '"+ textPassword.Text"'", con);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
